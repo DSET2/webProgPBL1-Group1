c896e7bbaae1f10588f034ea265b5f130ff60d78
Haikal Ahmad <58899364+Kal01-ai@users.noreply.github.com>
2021-04-19 21:35:30 +0800

    After login, this page will be displayed

---
+++ loginSuccessPage.php
@@ -0,0 +1,9 @@
+<html>
+
+  <body style="background-color:#455a64; color:#ffffff">
+	Welcome <?php echo $_POST["name"]; ?><br>
+	
+	Your password is <?php echo $_POST["passwd"]; ?>
+  </body>
+
+</html>

1e7344b1cc8c018558655366145abe3879275fc6
Kal01-ai <haikalahmad130@gmail.com>
2021-05-19 17:18:54 +0800

    Added DB connection and INSERT to DB
    DB connection
    
    INSERT data to DB

--- registrationcheck.php
+++ registrationcheck.php
@@ -17,7 +17,36 @@ if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 
 Username is: <?php echo $_POST["name"]; ?> <br> <br>
 Password is: <?php echo $_POST["pass"]; ?><br><br>
-Email is: <?php echo $_POST["email"]; ?>
+Email is: <?php echo $_POST["email"]; ?><br><br>
+
+<?php 
+$servername="localhost";
+$username="root";
+$password="";
+
+$dbase="purchaseFormDB";
+
+$conn=new mysqli($servername, $username, $password, $dbase);
+
+if ($conn->connect_error) {
+	die("Connection failed:" .$conn->connect_error);
+} else
+echo "Connect Success    ";
+
+$name = $_POST["name"];
+$password = $_POST["pass"];
+$email = $_POST["email"];
+
+$sql="INSERT INTO registration(username, password, email)
+VALUES('$name','$password','$email')";
+
+if($conn->query($sql)===TRUE) {
+	echo "New record created";
+} else {
+	echo "Error:" .$sql. "<br>" . $conn->error;
+}
+?>
+
 <?php
 header("Refresh:3; url = index.html");?>
 </body>

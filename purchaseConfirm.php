5be93957f3fc6cfa06dafb4000bd99cf046deeab
Ikmal Azman <opikmalazman@gmail.com>
2021-05-23 08:16:55 +0800

    Setup purchaseDB
    Failed to insert data into db

--- purchaseConfirm.php
+++ purchaseConfirm.php
@@ -1,3 +1,44 @@
+<?php
+
+      $serverName = "localhost";
+      $userName = "root";
+      $password = "";
+      $dbase = "purchaseFormDB";
+
+      # Connect to db
+      $conn = new mysqli($serverName, $userName, $password);
+      if ($conn->connect_error) {
+        die("Connection Failed" . $conn->connect_error);
+      } else {
+        echo "Connection to db success";
+      }
+
+      $firstName = $_POST["firstName"];
+      $lastName = $_POST["lastName"];
+      $phoneNumber = $_POST["phoneNumber"];
+      $email = $_POST["email"];
+      $streetAddress = $_POST["streetAddress"];
+      $city = $_POST["city"];
+      $state = $_POST["state"];
+      $zipCode = $_POST["zipCode"];
+      $itemPuchase = $_POST["item"];
+
+      #Insert Item into DB
+      $sql="INSERT INTO purchaseItem(firstName,lastName,phoneNumber,email,streetAddress,cityName,state,zipCode,itemName)
+      VALUES('$firstName','$lastName','$phoneNumber','$email','$streetAddress','$city','$state','$zipCode','$itemPuchase')
+      ";
+
+      if($conn->query($sql)===TRUE)
+      {
+        echo "New Record created";
+      }
+      else
+      {
+        echo "Error : ".$sql."<br>". $conn->error;
+      }
+
+      ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,27 +53,21 @@
     <h1>Purchasing success</h1>
     <?php
 
-    $firstName = $_POST["firstName"];
-    $lastName = $_POST["lastName"];
-    $phoneNumber= $_POST["phoneNumber"];
-    $email = $_POST["email"];
-    $streetAddress = $_POST["streetAddress"];
-    $city = $_POST["city"];
-    $state = $_POST["state"];
-    $zipCode = $_POST["zipCode"];
+
 
     echo "Name :  $firstName  $lastName  <br>";
     echo "Phone Number :  $phoneNumber <br>";
     echo "Email :  $email <br>";
+
     echo "Street Address :  $streetAddress <br>";
     echo "City :  $city <br>";
     echo "State :  $state <br>";
     echo "State :  $zipCode <br>";
 
 
-    header("Refresh:5; url = index.html");
+    // header("Refresh:5; url = index.html");
     ?>
 
 </body>
 
-</html>
+</html> -->

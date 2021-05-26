3d2e511a97b84fafff691bbbcfe383a9aa887610
Canerson <82663199+Canerson@users.noreply.github.com>
2021-05-26 15:19:29 +0800

    new register that has problems

--- registrationcheck.php
+++ registrationcheck.php
@@ -2,6 +2,7 @@
 <h1><u> Details</u></h1>
 <body>
 <?php
+
 if ($_POST["pass"]!= $_POST["cpassword"])
  {
      echo("Oops! Password did not match! Try again. ");
@@ -11,15 +12,21 @@ if ($_POST["pass"]!= $_POST["cpassword"])
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
  echo ("Invalid email format! Try again.");
   return;
+  
+  $name=$P_OST_["name"];
+  $password=$_POST["pass"];
+  $email=$_POST["email"];
 }
+  ?>
+
  
- ?>
+ 
 
 Username is: <?php echo $_POST["name"]; ?> <br> <br>
 Password is: <?php echo $_POST["pass"]; ?><br><br>
 Email is: <?php echo $_POST["email"]; ?><br><br>
 
-<?php 
+ <?php
 $servername="localhost";
 $username="root";
 $password="";
@@ -33,18 +40,19 @@ if ($conn->connect_error) {
 } else
 echo "Connect Success    ";
 
-$name = $_POST["name"];
-$password = $_POST["pass"];
-$email = $_POST["email"];
 
-$sql="INSERT INTO registration(username, password, email)
-VALUES('$name','$password','$email')";
 
-if($conn->query($sql)===TRUE) {
-	echo "New record created";
-} else {
-	echo "Error:" .$sql. "<br>" . $conn->error;
-}
+$sql="INSERT INTO registration (username,password, email)
+VALUES 
+('$name','$password','$email')";
+  if($conn->query($sql)===TRUE)
+      {
+        echo "New Record created";
+      }
+      else
+      {
+        echo "Error : ".$sql."<br>". $conn->error;
+      }
 ?>
 
 <?php
